cfcc1140624d866a80913e7f31ce1ec1e4c34bb6
Andrea Staskova <andrea@intiveo.com>
2021-04-15 16:26:19 +0200

    [FEATURE] loops added to print to console

--- main.js
+++ main.js
@@ -5,7 +5,9 @@
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+for (i = 0; i <= 10; i++) {
+    console.log(i)
+}
 
 console.log('-------------------');
 
@@ -17,7 +19,9 @@ console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
+for (i = 0; i < 10; i++) {
+    console.log(i)
+}
 
 console.log('-------------------');
 
@@ -26,6 +30,8 @@ console.log('-------------------');
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+for (i = 10; i >= 0; i--) {
+    console.log(i)
+}
 
 console.log('-------------------');
