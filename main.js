05721267d6e279c34d0ec9ca96ef2670ab9643c2
Andrea Staskova <andrea@intiveo.com>
2021-04-15 16:34:12 +0200

    [FEATURE] for loops changed to while loops

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-for (i = 0; i <= 10; i++) {
-    console.log(i)
+let i = 0;
+
+while (i <= 10) {
+    console.log(i) ;
+    i++;
 }
 
 console.log('-------------------');
-
-
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -19,19 +19,23 @@ console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-for (i = 0; i < 10; i++) {
-    console.log(i)
+let j = 0;
+
+while (j < 10) {
+    console.log(j) ;
+    j++;
 }
 
 console.log('-------------------');
 
-
-
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-for (i = 10; i >= 0; i--) {
-    console.log(i)
+let k = 10;
+
+while (k >= 0) {
+    console.log(k);
+    k--
 }
 
 console.log('-------------------');
